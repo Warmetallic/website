7408933b2f07c4431478b81551c0c36a7be86be3
Gleb Kharlamov <scream10@list.ru>
2021-09-01 19:09:25 +0100

    music

--- static/js/angryface.js
+++ static/js/angryface.js
@@ -1,7 +1,23 @@
-$(document).ready(function(){
-    $("#face").dblclick(function(){
-        var obj = document.createElement("audio");
-        obj.src = "mchammer.mp3"; 
-        obj.play(); 
-    });
-  });
+var audio = document.getElementById('audio');
+var playPauseBTN = document.getElementById('playPauseBTN');
+var count = 0;
+
+function playPause(){
+    if(count == 0){
+        count = 1;
+        audio.play();
+        playPauseBTN.innerHTML = "Pause ⏸";
+    }else{
+        count = 0;
+        audio.pause();
+        playPauseBTN.innerHTML = "Play ►";
+    }
+
+}
+
+function stop(){
+    playPause()
+    audio.pause();
+    audio.currentTime = 0;
+    playPauseBTN.innerHTML = "Play ►";
+}
