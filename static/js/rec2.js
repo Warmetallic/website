01e25ab79678553ad8a78e04a9c622e028dfc6e6
Gleb Kharlamov <scream10@list.ru>
2021-09-28 23:27:58 +0100

    rec

--- static/js/rec2.js
+++ static/js/rec2.js
@@ -1,9 +1,9 @@
 // Get the modal
-var modal = document.getElementById("myModal");
+var modal = document.getElementById("myModal2");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
+var img = document.getElementById("myImg2");
+var modalImg = document.getElementById("img02");
 var elements = document.getElementById("nav");
 img.onclick = function(){
   modal.style.display = "block";
@@ -12,7 +12,7 @@ img.onclick = function(){
 }
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("close")[1];
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
